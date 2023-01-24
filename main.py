6549cb0b462f9120826493b91fdb6b23c79d8960
Sheikh Abdul Munim <sheikh00munim@gmail.com>
2023-01-24 18:33:38 +0600

    Initial

--- main.py
+++ main.py
@@ -1,9 +1,89 @@
-def entry():
-    a = int(input('please enter a value'))
-    if a < 8:
-        print('Welcome to the com.')
+import pygame
+#import time
 
-    else:
-        print('getlost')
+pygame.init()
 
-entry()
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+
+dis_width = 800
+dis_height = 600
+
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Snake Game')
+
+clock = pygame.time.Clock()
+
+snake_block = 10
+snake_speed = 30
+
+font_style = pygame.font.SysFont(None, 50)
+
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width / 2, dis_height / 2])
+
+
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x1 = dis_width / 2
+    y1 = dis_height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_List = []
+    Length_of_snake = 1
+
+    while not game_over:
+
+        while game_close == True:
+            dis.fill(black)
+            message("You Lost! Press Q-Quit or C-Play Again", red)
+            Your_score(Length_of_snake - 1)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+                game_close = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+            game_close = True
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(black)
+        pygame.draw.rect(dis, white, [x1, y1, snake_block, snake_block])
+        pygame.display.update()
+
+        clock.tick(snake_speed)
+
+    pygame.quit()
+
+
+gameLoop()
